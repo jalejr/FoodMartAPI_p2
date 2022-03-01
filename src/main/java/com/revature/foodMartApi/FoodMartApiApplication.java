021b8ede24410586e91ba385524d683738dc7aca
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-02-28 19:29:54 -0800

    Skeleton project

---
+++ src/main/java/com/revature/foodMartApi/FoodMartApiApplication.java
@@ -0,0 +1,14 @@
+package com.revature.foodMartApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMartApiApplication.class, args);
+		System.out.println("Hey there");
+	}
+
+}
