e69ac24a0286fb16fc27fc176767753927e289d4
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-03-02 13:12:22 -0800

    GroceryDAO first draft with model tweaks and skele of service

---
+++ src/main/java/com/revature/foodMartApi/services/GroceryListService.java
@@ -0,0 +1,10 @@
+package com.revature.foodMartApi.services;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class GroceryListService {
+}
