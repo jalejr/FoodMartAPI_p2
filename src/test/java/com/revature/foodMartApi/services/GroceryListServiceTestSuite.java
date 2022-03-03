7c71adb03d835ec5c67332f8d866f3d018b3103b
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-03-02 18:07:55 -0800

    GroceryListTestSuite added findAllById and findAll

--- src/test/java/com/revature/foodMartApi/services/GroceryListServiceTestSuite.java
+++ src/test/java/com/revature/foodMartApi/services/GroceryListServiceTestSuite.java
@@ -5,6 +5,10 @@ import com.revature.foodMartApi.exceptions.InvalidRequestException;
 import com.revature.foodMartApi.models.GroceryList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.endpoint.web.Link;
+
+import java.util.LinkedList;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -48,11 +52,39 @@ class GroceryListServiceTestSuite {
     }
 
     @Test
-    void findAllGroceryLists() {
+    void findAllGroceryLists_givenData() {
+        LinkedList<GroceryList> groceryLists = new LinkedList<>();
+        groceryLists.add(new GroceryList());
+        groceryLists.add(new GroceryList());
+        groceryLists.add(new GroceryList());
+        groceryLists.add(new GroceryList());
+        groceryLists.add(new GroceryList());
+        groceryLists.add(new GroceryList());
+
+        when(mockGroceryListDAO.findAll()).thenReturn(groceryLists);
+        groceryLists = (LinkedList<GroceryList>) sut.findAllGroceryLists();
+
+        boolean actualResult = groceryLists.size() == 6;
+
+        assertTrue(actualResult);
+        verify(mockGroceryListDAO, times(1)).findAll();
     }
 
     @Test
-    void findGroceryById() {
+    void findGroceryById_givenGoodData() {
+        LinkedList<GroceryList> groceryLists = new LinkedList<>();
+        groceryLists.add(new GroceryList(1L, 2));
+        groceryLists.add(new GroceryList(2L, 1));
+        groceryLists.add(new GroceryList(3L, 4));
+        groceryLists.add(new GroceryList(4L, 5));
+        groceryLists.add(new GroceryList(5L, 2));
+        groceryLists.add(new GroceryList(6L, 7));
+
+        when(mockGroceryListDAO.findById(3L)).thenReturn(Optional.ofNullable(groceryLists.get(2)));
+        Optional<GroceryList> foundGroceryList = sut.findGroceryById(3L);
+
+        assertNotNull(foundGroceryList);
+        verify(mockGroceryListDAO, times(1)).findById(3L);
     }
 
     @Test
