18de9503f5fc9b4f8716aa350467e517d580024a
Awaab Elamin <AwaabElamin@gmail.com>
2022-03-02 18:31:47 -0600

    add role model wth table annotation and add mainCotroller for showing in a browser

--- src/main/java/com/revature/foodMartApi/FoodMartApiApplication.java
+++ src/main/java/com/revature/foodMartApi/FoodMartApiApplication.java
@@ -2,17 +2,9 @@ package com.revature.foodMartApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class FoodMartApiApplication {
-
-	@GetMapping("/test")
-	public String testEndPoint() {
-		return "Hey from the CI/CD pipeline! We did the thing!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodMartApiApplication.class, args);
---
+++ src/main/java/com/revature/foodMartApi/exceptions/ResourcePersistenceException.java
@@ -0,0 +1,10 @@
+package com.revature.foodMartApi.exceptions;
+
+@SuppressWarnings("serial")
+public class ResourcePersistenceException extends RuntimeException {
+
+	public ResourcePersistenceException(String message) {
+		super(message);
+	}
+	
+}
