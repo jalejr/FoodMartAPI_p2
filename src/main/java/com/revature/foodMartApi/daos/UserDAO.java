a77df33bb1d95dd333e22ce9e90017603d4eb288
Mike Campbell <70548966+maikamp@users.noreply.github.com>
2022-03-03 18:00:50 -0500

    added custom retrieval by username/email/username+pass for UserDAO

--- src/main/java/com/revature/foodMartApi/daos/UserDAO.java
+++ src/main/java/com/revature/foodMartApi/daos/UserDAO.java
@@ -1,11 +1,20 @@
 package com.revature.foodMartApi.daos;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 import com.revature.foodMartApi.models.User;
 
 @Repository
 public interface UserDAO extends CrudRepository<User, Integer>{
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    @Query("from User where User.username = :username and User.password = :password")
+    User findUserByUsernameAndPassword(String username, String password);
 
 }
