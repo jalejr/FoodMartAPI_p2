dace4f4e75225ec7f71f5649def7e92ac68af41a
Mike Campbell <70548966+maikamp@users.noreply.github.com>
2022-03-03 12:06:11 -0500

    added test stubs for all methods covering most (all?) cases in UserServiceTestSuite, updated UserService with findAll implementation

--- src/main/java/com/revature/foodMartApi/services/UserService.java
+++ src/main/java/com/revature/foodMartApi/services/UserService.java
@@ -1,5 +1,6 @@
 package com.revature.foodMartApi.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,69 +15,71 @@ import com.revature.foodMartApi.models.User;
 @Service
 @Transactional
 public class UserService {
-	private final UserDAO userDAO;
-	
-	@Autowired
-	public UserService(UserDAO userDAO) {
-		this.userDAO = userDAO;
-	}
-	
-	
-	public User addUser(User user) {
-		//TODO 
-		if (!isValidUser(user)) {
-			throw new InvalidRequestException("Invalid user provided.");
-		}
-		User persistedUser = userDAO.save(user);
-        if(persistedUser == null){
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public User addUser(User user) {
+        if (!isValidUser(user)) {
+            throw new InvalidRequestException("Invalid user data provided.");
+        }
+        User persistedUser = userDAO.save(user);
+        if (persistedUser == null) {
             throw new ResourcePersistenceException("User was not persisted.");
         }
-    
-		return user;
-	}
-	
-	public List<User> findAllUsers (){
-		//TODO
-		return null;
-	}
-	
-	public User findUserById (){
-		//TODO
-		return null;
-	}
-	
-	public User findUserByUsername (){
-		//TODO
-		return null;
-	}
-	
-	public User findUserByEmail (){
-		//TODO
-		return null;
-	}
-	
-	public User authenticateUser () {
-		//TODO		
-		return null;
-	}
-	
-	public boolean updateUser () {
-		//TODO		
-		return false;
-	}
-	
-	public boolean deleteUser () {
-		//TODO		
-		return false;
-	}
-	
-	private boolean isValidUser(User user) {
-		//TODO add more validation constraints
-		if(user==null) {
-		return false;
-		}
-		else {
-			return true;
-		}
-	}
+        return user;
+    }
+
+    public List<User> findAllUsers() {
+        // TODO
+        List<User> users = new ArrayList<>();
+        //iterate through iterable and add to users
+        Iterable<User> userIterable = userDAO.findAll();
+        for(User user : userIterable){
+            users.add(user);
+        }
+        return users;
+    }
+
+    public User findUserById(int id) {
+        // TODO
+        return null;
+    }
+
+    public User findUserByUsername(String username) {
+        // TODO
+        return null;
+    }
+
+    public User findUserByEmail(String email) {
+        // TODO
+        return null;
+    }
+
+    public User authenticateUser(User user) {
+        // TODO
+        return null;
+    }
+
+    public boolean updateUser(User user) {
+        // TODO
+        return false;
+    }
+
+    public boolean deleteUser(User user) {
+        // TODO
+        return false;
+    }
+
+    private boolean isValidUser(User user) {
+        // TODO add more validation constraints
+        if (user == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
--- src/test/java/com/revature/foodMartApi/services/UserServiceTestSuite.java
+++ src/test/java/com/revature/foodMartApi/services/UserServiceTestSuite.java
@@ -5,6 +5,9 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -37,12 +40,38 @@ class UserServiceTestSuite {
 	}
 
 	@Test
-	void test_addUser_throwsInvalidRequestException_givenInvalidUser() {
+	void test_addUser_throwsInvalidRequestException_givenNullUser() {
 		InvalidRequestException thrown = Assertions.assertThrows(InvalidRequestException.class, () -> {
-			// Code under test
 			sut.addUser(null);
 		});
-		Assertions.assertEquals("Invalid user provided.", thrown.getMessage());
+		Assertions.assertEquals("Invalid user data provided.", thrown.getMessage());
+	}
+
+	@Test
+	void test_addUser_throwsInvalidRequestException_givenInvalidUsername() {
+		InvalidRequestException thrown = Assertions.assertThrows(InvalidRequestException.class, () -> {
+			// TODO create user with invalid username
+			sut.addUser(null);
+		});
+		Assertions.assertEquals("Invalid user data provided.", thrown.getMessage());
+	}
+
+	@Test
+	void test_addUser_throwsInvalidRequestException_givenInvalidEmail() {
+		InvalidRequestException thrown = Assertions.assertThrows(InvalidRequestException.class, () -> {
+			// TODO create user with invalid email
+			sut.addUser(null);
+		});
+		Assertions.assertEquals("Invalid user data provided.", thrown.getMessage());
+	}
+
+	@Test
+	void test_addUser_throwsInvalidRequestException_givenInvalidPassword() {
+		InvalidRequestException thrown = Assertions.assertThrows(InvalidRequestException.class, () -> {
+			// TODO create user with invalid password
+			sut.addUser(null);
+		});
+		Assertions.assertEquals("Invalid user data provided.", thrown.getMessage());
 	}
 
 	@Test
@@ -59,4 +88,94 @@ class UserServiceTestSuite {
 		verify(mockUserDAO, times(1)).save(validUser);
 	}
 
+	@Test
+	void test_findAllUsers_returnsCollection() {
+		List<User> users = new ArrayList<>();
+
+		when(mockUserDAO.findAll()).thenReturn(users);
+
+		boolean actualResult = !(sut.findAllUsers() == null);
+
+		Assertions.assertTrue(actualResult);
+		verify(mockUserDAO, times(1)).findAll();
+	}
+
+	// @Test
+	// void test_findAllUsers_returnsNull() {
+	// List<User> users = new ArrayList<>();
+
+	// when(mockUserDAO.findAll()).thenReturn(users);
+
+	// boolean actualResult = !(sut.findAllUsers() == null);
+
+	// Assertions.assertTrue(actualResult);
+	// verify(mockUserDAO, times(1)).findAll();
+	// }
+	@Test
+	void test_findUserById_returnsUser_givenValidId() {
+
+	}
+	@Test
+	void test_findUserById_returnsNull_givenInvalidId() {
+
+	}
+
+	@Test
+	void test_findUserByUsername_returnsUser_givenValidUsername() {
+
+	}
+	@Test
+	void test_findUserByUsername_returnsNull_givenInvalidUsername() {
+
+	}
+
+	@Test
+	void test_findUserByEmail_returnsUser_givenValidEmail() {
+
+	}
+
+	@Test
+	void test_findUserByEmail_returnsNull_givenInvalidEmail() {
+
+	}
+
+	@Test
+	void test_authenticateUser_returnsUser_givenValidUser() {
+
+	}
+
+	@Test
+	void test_authenticateUser_throwsAuthenticationException_givenInvalidUser() {
+
+	}
+
+	@Test
+	void test_updateUser_returnsTrue_givenValidUser() {
+
+	}
+	//throw ResourcePersistenceException?
+	@Test
+	void test_updateUser_returnsFalse_givenInvalidUser() {
+
+	}
+
+	@Test
+	void test_deleteUser_returnsTrue_givenValidUser() {
+
+	}
+	//throw ResourcePersistenceException?
+	@Test
+	void test_deleteUser_returnsFalse_givenInvalidUser() {
+
+	}
+
+	@Test
+	void test_isValidUser_returnsTrue_givenValidUser() {
+
+	}
+
+	@Test
+	void test_isValidUser_returnsFalse_givenInvalidUser() {
+
+	}
 }
