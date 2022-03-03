3b168b456776b35d9388cebb32c44e45a24b34f9
Awaab Elamin <AwaabElamin@gmail.com>
2022-03-03 14:47:16 -0600

    update servlet of role to cover all CRUD

--- src/main/java/com/revature/foodMartApi/servlets/RoleServlet.java
+++ src/main/java/com/revature/foodMartApi/servlets/RoleServlet.java
@@ -6,10 +6,14 @@ package com.revature.foodMartApi.servlets;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,11 +36,40 @@ public class RoleServlet {
 		this.roleService = roleService;
 	}
 	
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	@ResponseBody
+	public Role createRole(@RequestBody String description) {		
+		return roleService.createRole(description);
+	}
+	
+	@GetMapping("/byId")
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public Role findRoleById(@RequestParam int id) {
+		return roleService.findRoleById(id);
+	}
+	
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
 	public List<Role> getAllRoles(){
-		return roleService.getAllRoles();
+		return (List<Role>)roleService.getAllRoles();
 	}
+	
+	@PutMapping
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public Role updateAvatarItem(@RequestBody Role role) {
+		return roleService.updateRole(role);
+	}
+	
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void deleteAvatarItem(@RequestParam int id) {
+		roleService.deleteRole(id);
+	}
+	
 	
 }
