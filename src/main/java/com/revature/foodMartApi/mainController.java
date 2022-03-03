af647299ea392be01853ce7572e05160f11ed2fb
Awaab Elamin <AwaabElamin@gmail.com>
2022-03-02 21:56:05 -0600

    add some helpful file to testing runnimg

--- src/main/java/com/revature/foodMartApi/mainController.java
+++ src/main/java/com/revature/foodMartApi/mainController.java
@@ -6,16 +6,20 @@ package com.revature.foodMartApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.foodMartApi.services.RoleService;
+
 /**
  * @author Awaab
  *
  */
-@RestController
-public class mainController {
-	
-	@GetMapping("/")
-	public String index() {
-		return "Greeting";
-	}
-
-}
+//@RestController
+//public class mainController {
+//	private RoleService roleService;
+//	
+//	@GetMapping("/")
+//	public String index() {
+//		roleService.getAllRoles();
+//		return roleService.getAllRoles().toString();
+//	}
+//
+//}
