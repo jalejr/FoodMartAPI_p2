e522dcf35ce8397323b2b44c88ad2ae21e0728fc
Awaab Elamin <AwaabElamin@gmail.com>
2022-03-04 14:10:09 -0600

    update servlet

--- src/main/java/com/revature/foodMartApi/servlets/RoleServlet.java
+++ src/main/java/com/revature/foodMartApi/servlets/RoleServlet.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import com.revature.foodMartApi.services.RoleService;
  */
 @RestController
 @RequestMapping("/roles")
+@CrossOrigin
 public class RoleServlet {
 	private final RoleService roleService;
 
