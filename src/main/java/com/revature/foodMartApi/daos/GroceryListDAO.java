474f1fa15d200af246d9d82ad5917ba317d91cc1
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-03-04 18:08:53 -0800

    GroceryListDAO added FindByListId into DAO

--- src/main/java/com/revature/foodMartApi/daos/GroceryListDAO.java
+++ src/main/java/com/revature/foodMartApi/daos/GroceryListDAO.java
@@ -1,10 +1,14 @@
 package com.revature.foodMartApi.daos;
 
 import com.revature.foodMartApi.models.GroceryList;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GroceryListDAO extends CrudRepository<GroceryList, Long> {
-
+    @Query("FROM GroceryList i WHERE i.listId =: listId")
+    Optional<GroceryList> findGroceryListByListId(int listId);
 }
