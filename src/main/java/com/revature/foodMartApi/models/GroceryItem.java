7dadabd65908a53c031b56d6381349e237f8cba9
Scott Damon <scott.m.damon1@gmail.com>
2022-03-09 12:01:15 -0500

    Fixed issues with GroceryItem model/service/testing not persisiting

--- src/main/java/com/revature/foodMartApi/models/GroceryItem.java
+++ src/main/java/com/revature/foodMartApi/models/GroceryItem.java
@@ -1,5 +1,7 @@
 package com.revature.foodMartApi.models;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,6 +18,9 @@ public class GroceryItem {
 	@Column(name="item_id")
 	private int itemId;
 	
+	@Column(name="item_name")
+	private String itemName;
+	
 	@Column(name="description")
 	private String description;
 	
@@ -29,42 +34,107 @@ public class GroceryItem {
 		super();
 	}
 
-	public GroceryItem(int itemId, String description, double itemPrice, int inventoryCount) {
+
+
+	public GroceryItem(int itemId, String itemName, String description, double itemPrice, int inventoryCount) {
+		super();
 		this.itemId = itemId;
+		this.itemName = itemName;
 		this.description = description;
 		this.itemPrice = itemPrice;
 		this.inventoryCount = inventoryCount;
 	}
 
+
+
 	public int getItemId() {
 		return itemId;
 	}
+
+
 
 	public void setItemId(int itemId) {
 		this.itemId = itemId;
 	}
 
+
+
+	public String getItemName() {
+		return itemName;
+	}
+
+
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+
+
 	public String getDescription() {
 		return description;
 	}
+
+
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+
+
 	public double getItemPrice() {
 		return itemPrice;
 	}
+
+
 
 	public void setItemPrice(double itemPrice) {
 		this.itemPrice = itemPrice;
 	}
 
-	public int getInventory_count() {
+
+
+	public int getInventoryCount() {
 		return inventoryCount;
 	}
 
-	public void setInventory_count(int inventory_count) {
-		this.inventoryCount = inventory_count;
+
+
+	public void setInventoryCount(int inventoryCount) {
+		this.inventoryCount = inventoryCount;
 	}
+
+
+
+	@Override
+	public String toString() {
+		return "GroceryItem [itemId=" + itemId + ", itemName=" + itemName + ", description=" + description
+				+ ", itemPrice=" + itemPrice + ", inventoryCount=" + inventoryCount + "]";
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, inventoryCount, itemId, itemName, itemPrice);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GroceryItem other = (GroceryItem) obj;
+		return Objects.equals(description, other.description) && inventoryCount == other.inventoryCount
+				&& itemId == other.itemId && Objects.equals(itemName, other.itemName)
+				&& Double.doubleToLongBits(itemPrice) == Double.doubleToLongBits(other.itemPrice);
+	}
+	
+	
 }
