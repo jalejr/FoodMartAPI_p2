10753aa65c90f4da29df0b5769727b1e809207d8
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-03-10 13:49:21 -0800

    GroceryItemServlet documentation

--- src/main/java/com/revature/foodMartApi/web/servlets/GroceryItemServlet.java
+++ src/main/java/com/revature/foodMartApi/web/servlets/GroceryItemServlet.java
@@ -9,15 +9,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ *GroceryItem servlet with various end points for API access
+ */
 @RestController
 @RequestMapping("/groceryItems")
 public class GroceryItemServlet {
 
     private final GroceryItemService groceryItemService;
 
+    /**
+     * @param groceryItemService
+     * Constructor
+     */
     @Autowired
     public GroceryItemServlet(GroceryItemService groceryItemService) { this.groceryItemService = groceryItemService; }
 
+    /**
+     * @param groceryItem
+     * @return
+     * creates a grocery item and returns it. Takes a groceryList json
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
@@ -25,14 +37,27 @@ public class GroceryItemServlet {
         return groceryItemService.addGroceryItem(groceryItem);
     }
 
+    /**
+     * @return
+     * finds all GroceryItems and returns as List
+     */
     @GetMapping
     public List<GroceryItem> findAllGroceryItems() { return groceryItemService.findAllItems(); }
 
+    /**
+     * @param id
+     * @return
+     * finds GroceryItem by id and returns it
+     */
     @GetMapping("/id")
     public Optional<GroceryItem> findGroceryItemById(@RequestParam int id) {
         return groceryItemService.findItemById(id);
     }
 
+    /**
+     * @param groceryItem
+     * deletse groceryItem and wants a groceryItem json
+     */
     @DeleteMapping
     @ResponseStatus(HttpStatus.OK)
     public void deleteGroceryItem(@RequestBody GroceryItem groceryItem) {
