1e72461bdb7f6fa1eaae01d5ac6db47676d57c7c
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-03-10 14:02:57 -0800

    RoleServlet documentation

--- src/main/java/com/revature/foodMartApi/web/servlets/RoleServlet.java
+++ src/main/java/com/revature/foodMartApi/web/servlets/RoleServlet.java
@@ -24,7 +24,7 @@ import com.revature.foodMartApi.services.RoleService;
 
 /**
  * @author Awaab
- *
+ *RoleServlet handling mapping and requests
  */
 @RestController
 @RequestMapping("/roles")
@@ -32,41 +32,68 @@ import com.revature.foodMartApi.services.RoleService;
 public class RoleServlet {
 	private final RoleService roleService;
 
+	/**
+	 * @param roleService
+	 * construction
+	 */
 	@Autowired
 	public RoleServlet(RoleService roleService) {
 		super();
 		this.roleService = roleService;
 	}
-	
 
+
+	/**
+	 * @param role
+	 * @return
+	 * creates role and returns role
+	 */
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	@ResponseBody
 	public Role createRole(@RequestBody Role role) {
 		return roleService.createRole(role);
 	}
-	
+
+	/**
+	 * @param id
+	 * @return
+	 * finds role by Id and returns it
+	 */
 	@GetMapping("/byId")
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
 	public Role findRoleById(@RequestParam int id) {
 		return roleService.findRoleById(id);
 	}
-	
+
+	/**
+	 * @return
+	 * get all the roles and return them in a List
+	 */
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
 	public List<Role> getAllRoles(){
 		return (List<Role>)roleService.getAllRoles();
 	}
-	
+
+	/**
+	 * @param role
+	 * @return
+	 * update a role and returns it
+	 */
 	@PutMapping
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
 	public Role updateAvatarItem(@RequestBody Role role) {
 		return roleService.updateRole(role);
 	}
-	
+
+	/**
+	 * @param id
+	 * deletes a role using Id
+	 */
 	@DeleteMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void deleteAvatarItem(@RequestParam int id) {
