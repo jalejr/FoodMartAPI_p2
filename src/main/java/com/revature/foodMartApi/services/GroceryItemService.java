2d5273bfe0bff7ca2a081970a6355a29dc0294eb
Joshua R <32827900+jalejr@users.noreply.github.com>
2022-03-10 12:30:45 -0800

    GroceryItemService documentation

--- src/main/java/com/revature/foodMartApi/services/GroceryItemService.java
+++ src/main/java/com/revature/foodMartApi/services/GroceryItemService.java
@@ -11,15 +11,27 @@ import com.revature.foodMartApi.models.GroceryItem;
 import com.revature.foodMartApi.daos.GroceryItemDAO;
 import com.revature.foodMartApi.exceptions.InvalidRequestException;
 
+/**
+ * GroceryItemService
+ */
 @Service
 public class GroceryItemService {
 	private final GroceryItemDAO groceryItemDAO;
 
+	/**
+	 * @param groceryItemDAO
+	 * constructor which requires DAO
+	 */
 	@Autowired
 	public GroceryItemService(GroceryItemDAO groceryItemDAO) {
 		this.groceryItemDAO = groceryItemDAO;
 	}
 
+	/**
+	 * @param item
+	 * @return
+	 * Add a GroceryItem to the db. Has some checks to make sure data is valid. Also returns the grocery item put in
+	 */
 	@Transactional
 	public GroceryItem addGroceryItem(GroceryItem item) {
 		if (!isValidItem(item))  throw new InvalidRequestException("Invalid item provided.");
@@ -29,18 +41,28 @@ public class GroceryItemService {
 
 	}
 
+	/**
+	 * @return
+	 * Gives back a List of all GroceryItems in the DB
+	 */
 	public List<GroceryItem> findAllItems() {
 		return (List<GroceryItem>) groceryItemDAO.findAll();
 	}
 
+	/**
+	 * @param id
+	 * @return
+	 * Gives an GroceryItem back that's identified by the id
+	 */
 	public Optional<GroceryItem> findItemById(int id) {
 		return groceryItemDAO.findById(id);
 	}
 
-//	public String findByName(String itemName) {
-//		return  groceryItemDAO.findByName(itemName);
-//	}
-
+	/**
+	 * @param item
+	 * @return
+	 * A bool is returned depending if the GroceryItem is valid
+	 */
 	public boolean isValidItem(GroceryItem item) {
 		// TODO add more validation constraints
 		if (item == null) {
@@ -56,6 +78,11 @@ public class GroceryItemService {
 		return true;
 	}
 
+	/**
+	 * @param validItem
+	 * @return
+	 * Deletes a GroceryItem and returns if the delete was successful
+	 */
 	public boolean deleteGroceryItem(GroceryItem validItem) {
 		int id = validItem.getItemId();
 		groceryItemDAO.delete(validItem);
